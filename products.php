985d2cbc0bd1680ad16157c90c279f2c201d52d3
ajvazialtin1 <aa52392@ubt-uni.net>
2023-02-12 22:42:29 +0100

    editimi i pricer

--- products.php
+++ products.php
@@ -20,51 +20,74 @@ $conn->close();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/product.css">
-    <title>Subscribe</title>
+    <title></title>
     <?php
   include "./header.html"
 ?>
 </head>
-
-<body>
-    <article>
-    <div class="container" style="width: 100%;">
-
-       <?php while($rows=$result->fetch_assoc())
+<?php while($rows=$result->fetch_assoc())
    { ?> 
-        
-       <div class="priceing-table-main">
-            <div class="col-md-3 price-grid">
-          <div class="price-block agile">
-            <div class="price-gd-top pric-clr1"> 
-              <h4>Basic Plan</h4>
-              <h3>$10</h3>
-              <h5>Per Month</h5>
-            </div>
-            <div class="price-gd-bottom">
-              <div class="price-list">
-                <ul>
-                  <li><?php echo $rows['nameDoctor'];?></li>
-                  <li><?php echo $rows['maxPersona'];?></li>
-                  <li><?php echo $rows['minute'];?></li>
-                  <li><?php echo $rows['price'];?></li>
-                </ul>
-              </div>
-              <div class="price-selet pric-sclr1">
-                <a href="register.php" data-toggle="modal" data-target="#myModal1" >Sign Up</a> 
-              </div>
-            </div>
-          </div>
-           </div>  <?php } ?>
+<body>
+<main>
+</main>
+<br>
 
-</article>
+    <div class="container">
+    <div class="price">
+          <h3><?php echo $rows['name'];?></h3>
+          <p><?php echo $rows['nameDoctor'];?></p>
+          <p><?php echo $rows['maxPersona'];?></p>
+          <p><?php echo $rows['minute'];?></p>
+          <p><?php echo $rows['price'];?></p>
+          <button><a href="register.php"><input type="submit" value="regjistrohu"></button>
+    </div>
+  </div>
+<?php } ?> 
 
 
-  
+   </body>
+   <br>
   <?php
   include "./footer.html"
 ?>
-</article>
-   </body>
+
 </html>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Exo+2|Yatra+One');
+
+*{
+  font-family: 'Exo 2', sans-serif;
+}
+
+
+.price {
+  background-color: orange;
+  margin-left: 40%;
+  margin-right: 40%;
+  padding: 10px 0px;
+   text-align: center;
+}
+
+
+.price h3{
+  background-color: rgb(43, 120, 221);
+  color: white;
+  text-transform: uppercase;
+  line-height: 45px;
+  letter-spacing: 0px;
+
+}
+
+
+.price  p{
+  margin-top: 20px;
+  font-size: 14px;
+  line-height: 20px;
+  padding: 10px 0;
+  border-bottom: 1px solid #ebebeb;
+  color: white;
+}
+
+
+
+</style> 
